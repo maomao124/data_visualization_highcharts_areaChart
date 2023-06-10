6ed668dfe2e8a4c297609d9d80daa60569d937a8
mao <1296193245@qq.com>
2023-06-10 22:19:41 +0800

    data_visualization_highcharts_areaChart

---
+++ src/main/java/mao/data_visualization_highcharts_areachart/DataVisualizationHighchartsAreaChartApplication.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_highcharts_areachart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisualizationHighchartsAreaChartApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DataVisualizationHighchartsAreaChartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/data_visualization_highcharts_areachart/DataVisualizationHighchartsAreaChartApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_highcharts_areachart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataVisualizationHighchartsAreaChartApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
